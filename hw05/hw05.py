d7d3b2693980b6ce107aacd8ed0d14c60f1f524c
George YU <yumocoding@gmail.com>
2024-01-19 16:48:34 +0800

    Homework: Solve Q1 in Homework 5

--- hw05/hw05.py
+++ hw05/hw05.py
@@ -8,7 +8,13 @@ def hailstone(n):
     >>> next(hail_gen)
     1
     """
-    "*** YOUR CODE HERE ***"
+    yield n
+    if n == 1:
+        yield from hailstone(n)
+    elif n % 2 == 0:
+        yield from hailstone(n // 2)
+    else:
+        yield from hailstone(n * 3 + 1)
 
 
 def merge(a, b):
