dfa5c8294c9b3c7fbbddef311e177bfb4b58ee84
George YU <yumocoding@gmail.com>
2024-01-25 19:49:29 +0800

    Homework: Solve Q3 in Homework 6

--- hw06/hw06.py
+++ hw06/hw06.py
@@ -95,7 +95,11 @@ def store_digits(n):
     >>> cleaned = re.sub(r"#.*\\n", '', re.sub(r'"{3}[\s\S]*?"{3}', '', inspect.getsource(store_digits)))
     >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
     """
-    "*** YOUR CODE HERE ***"
+    result = Link.empty
+    while n != 0:
+        result = Link(n % 10, result)
+        n //= 10
+    return result
 
 
 def deep_map_mut(func, lnk):
