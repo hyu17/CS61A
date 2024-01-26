3a392e08d129cfc0c3aff8b6633ec615ba897555
George YU <yumocoding@gmail.com>
2024-01-26 17:05:33 +0800

    Homework: Solve Q4 in Homework 6

--- hw06/hw06.py
+++ hw06/hw06.py
@@ -121,8 +121,13 @@ def deep_map_mut(func, lnk):
     >>> print(link1)
     <9 <16> 25 36>
     """
-    "*** YOUR CODE HERE ***"
-
+    if lnk == Link.empty:
+        return
+    elif isinstance(lnk.first, Link):
+        deep_map_mut(func, lnk.first)
+    else:
+        lnk.first = func(lnk.first)
+    deep_map_mut(func, lnk.rest)
 
 def two_list(vals, counts):
     """
