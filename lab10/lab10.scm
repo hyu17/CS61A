3300917d2d0e89c95bb8cbf29a7ee390ec635037
George YU <yumocoding@gmail.com>
2024-02-02 20:50:12 +0800

    Lab: Solve Q2 in Lab 10

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,5 +1,9 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
-
+(define (over-or-under num1 num2)
+  (cond
+    ((< num1 num2) -1)
+     ((= num1 num2) 0)
+     ((> num1 num2) 1)
+  ))
 (define (make-adder num) 'YOUR-CODE-HERE)
 
 (define (composed f g) 'YOUR-CODE-HERE)
