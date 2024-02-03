6590df249a19de4f80301141892a18e66c76486c
George YU <yumocoding@gmail.com>
2024-02-03 20:12:32 +0800

    Homework: Solve Q3 in Homework 8

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -14,6 +14,12 @@
     )
 )
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    )
+)
 
 (define (no-repeats s) 'YOUR-CODE-HERE)
