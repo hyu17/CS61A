3202aa4cdf140c89610b0cbb786aa5fd60152338
George YU <yumocoding@gmail.com>
2024-02-20 11:40:41 +0800

    Project: Solve Problem 6 in Scheme

--- scheme/scheme_eval_apply.py
+++ scheme/scheme_eval_apply.py
@@ -86,7 +86,13 @@ def eval_all(expressions, env):
     2
     """
     # BEGIN PROBLEM 6
-    return scheme_eval(expressions.first, env) # replace this with lines of your own code
+    if expressions is nil:
+        return None
+    value = None
+    while expressions is not nil:
+        value = scheme_eval(expressions.first, env)
+        expressions = expressions.rest
+    return value
     # END PROBLEM 6
 
 
