dde40e2fc2d9811b66c5950f84e8398bf90f52f7
George YU <yumocoding@gmail.com>
2024-02-20 15:06:13 +0800

    Project: Solve Problem 9 in Scheme

--- scheme/scheme_eval_apply.py
+++ scheme/scheme_eval_apply.py
@@ -61,7 +61,8 @@ def scheme_apply(procedure, args, env):
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
     elif isinstance(procedure, LambdaProcedure):
         # BEGIN PROBLEM 9
-        "*** YOUR CODE HERE ***"
+        child_frame = procedure.env.make_child_frame(procedure.formals, args)
+        return eval_all(procedure.body, child_frame)
         # END PROBLEM 9
     elif isinstance(procedure, MuProcedure):
         # BEGIN PROBLEM 11
